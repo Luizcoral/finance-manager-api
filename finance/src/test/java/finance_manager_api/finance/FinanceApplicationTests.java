82f29ea625c4a0f42a6630c23268f404d91c5325
Luiz Coral <luizcoral012@gmail.com>
2026-03-23 20:18:52 -0300

    first commit

---
+++ finance/src/test/java/finance_manager_api/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package finance_manager_api.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
